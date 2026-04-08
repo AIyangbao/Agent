471765c8f8127ad981a58b1dca6d6fd31c60e0ef
流月啊！好好努力 <194564638@qq.com>
2026-04-08 22:53:55 +0800

    FastAPI路径函数

--- FastAPI_learning/first_learning_fastAPI.py
+++ FastAPI_learning/first_learning_fastAPI.py
@@ -11,15 +11,17 @@ async def root_test():
 async def sat_hello_test(name:str):
     return {"message":f"hello {name}"}
 
-@app.get('/')
-async def root():
+@app.get('/FasAPI')
+async def get_FastAPI():
     return {"msg":"你好,FastAPI!"}
 
 @app.get('/book/{id}')
 async def get_book(id: int):
     return {"id":id,"title":f"这是第{id}本书"}
 
-
+@app.get('/user_id/{id}')
+async def get_user_id(id:int):
+    return {"id":id,"user_id":f"普通用户{id}"}
 if __name__ == '__main__':
     uvicorn.run(app,host="127.0.0.1",port=8000)
 
