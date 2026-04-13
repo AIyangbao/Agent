a8711d3eac63fb7dd958a91d6926a263af86314b
流月啊！好好努力 <194564638@qq.com>
2026-04-13 16:33:20 +0800

    补充练习案例

--- FastAPI_learning/FastAPI基础/FastAPI请求体参数.py
+++ FastAPI_learning/FastAPI基础/FastAPI请求体参数.py
@@ -10,9 +10,18 @@ class Book(BaseModel):
     publisher:str=Field("黑马出版社")
     sell_price:int=Field(...,gt=0)
 
-@app.get('/book/information')
+@app.post('/book/information')
 async def get_book_information(book=Book):
     return {book}
+
+# 注册: 用户名和密码 -> str
+class User(BaseModel):
+    username: str = Field(default="张三",min_length=2,max_length=10,description="用户名,长度限制为0~10")
+    password: str = Field(min_length=3,max_length=20)
+
+@app.post("/register")
+async def register(user: User):
+    return user
 
 if __name__ == "__main__":
     uvicorn.run(app,host="127.0.0.1",port=8000)
