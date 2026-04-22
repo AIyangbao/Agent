75ef1e4d5293b5c95cb9e90ab56df886f7baead5
流月啊！好好努力 <194564638@qq.com>
2026-04-22 21:25:58 +0800

    挂载用户模块路由接口

--- toutiao_news/toutiao_backend/main.py
+++ toutiao_news/toutiao_backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import news
+from routers import news,users
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -19,6 +19,7 @@ async def root():
 
 # 挂载路由/注册路由
 app.include_router(news.router)
+app.include_router(users.router)
 
 if __name__ == "__main__":
     uvicorn.run(app,host="127.0.0.1",port=8000)
