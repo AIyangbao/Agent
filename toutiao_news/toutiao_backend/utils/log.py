233014263695885b947e07fbffee4df9aa529457
流月啊！好好努力 <194564638@qq.com>
2026-06-05 22:37:31 +0800

    头条新闻项目日志

---
+++ toutiao_news/toutiao_backend/utils/log.py
@@ -0,0 +1,51 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+# 日志目录
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR,exist_ok=True)
+
+# 日志格式 (时间、日志器名、级别、消息)
+LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
+
+def setup_log():
+    logger = logging.getLogger("toutiao_backend")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    # 避免重复添加 handler
+    if logger.handler:
+        return logger
+    
+    # 1. 普通日志文件 (按大小切割)
+    file_handler = RotatingFileHandler(
+        filename=os.path.join(LOG_DIR,"app.log"),
+        maxBytes=50*1024*1024,
+        backupCount=5,
+    )
+    file_handler.setFormatter(logging.Formatter(LOG_FORMAT,DATE_FORMAT))
+
+    # 2. 错误日志单存在 error.log
+    error_handler = RotatingFileHandler(
+        filename=os.path.join(LOG_DIR,"error.log"),
+        maxBytes=50 * 1024 * 1024,
+        backupCount=5,
+        encoding="utf-8"
+    )
+    error_handler.setlevel(logging.ERROR)
+    error_handler.setFormatter(logging.Formatter(LOG_FORMAT,DATE_FORMAT))
+
+    # 3.控制台输出
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(logging.Formatter( LOG_FORMAT,DATE_FORMAT))
+
+    logger.addHandler(file_handler)
+    logger.addHandler(error_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+# 全局 logger对象
+logger = setup_log()
