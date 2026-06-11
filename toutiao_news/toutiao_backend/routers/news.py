82591cf6574830bcfe4860a060d99ea6c8c25484
流月啊！好好努力 <194564638@qq.com>
2026-06-11 12:25:19 +0800

    统一成功响应回复格式

--- toutiao_news/toutiao_backend/routers/news.py
+++ toutiao_news/toutiao_backend/routers/news.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from config.db_conf import get_db
 from crud import news
 from crud import news_cache
+from utils.response import success_response
 # 创建 APIRouter 实例
 router = APIRouter(prefix="/api/news",tags=["news"])
 
@@ -15,17 +16,13 @@ router = APIRouter(prefix="/api/news",tags=["news"])
 async def get_categories(skip: int =0 , limit: int=100,db:AsyncSession = Depends(get_db)):
     # 先获取数据库里面新闻分类数据 -> 定义模类型 -> 封装查询数据的方法
     categories = await news_cache.get_categories(db,skip,limit)
-    return {
-            "code":200,
-            "message":"获取新闻分类成功",
-            "data":categories
-            }
+    return success_response(message="获取新闻分类成功",data=categories)
 
 @router.get("/list")
 async def get_news_list(
     category_id: int = Query(...,alias="categoryId"),
     page: int =1,
-    page_size: int = Query(10,alias="pageSzie",le=100),
+    page_size: int = Query(10,alias="pageSize",le=100),
     db: AsyncSession = Depends(get_db)
 ):
     # 思路: 处理分页规则 -> 查询新闻列表 -> 计算总量 -> 计算是否还有更多
@@ -34,15 +31,12 @@ async def get_news_list(
     total = await news.get_news_count(db,category_id)
     # (跳过的 + 当前列表里面的数量) < 总量
     has_more = (offset + len(news_list)) < total
-    return {
-        "code": 200,
-        "message": "获取新闻列表成功",
-        "data": {
+    data = {
             "list": news_list,
             "total": total,
             "hasMore": has_more
         }
-    }
+    return success_response(message="获取新闻列表成功",data=data)
 
 @router.get("/detail")
 async def get_news_detail(news_id: int = Query(...,alias="id"),db:AsyncSession = Depends(get_db)):
@@ -56,10 +50,7 @@ async def get_news_detail(news_id: int = Query(...,alias="id"),db:AsyncSession =
         raise HTTPException(status_code=404,detail="新闻不存在")
     
     related_news = await news_cache.get_related_news(db,news_detail.id,news_detail.category_id)
-    return {
-        "code": 200,
-        "message":"success",
-        "data": {
+    data = {
             "id": news_detail.id,
             "title": news_detail.title,
             "content": news_detail.content,
@@ -68,7 +59,7 @@ async def get_news_detail(news_id: int = Query(...,alias="id"),db:AsyncSession =
             "publishTime": news_detail.publish_time,
             "views": news_detail.views,
             "relatedNews": related_news      
-            }
-    }
+            } 
+    return success_response(message="获取新闻详情成功",data=data)
 
 
