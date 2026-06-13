5cf4eac6214ec9675c696aed0f36552705d4909f
流月啊！好好努力 <194564638@qq.com>
2026-06-13 18:58:44 +0800

    删去错误的测试接口

--- toutiao_news/toutiao_backend/tests/test_users.py
+++ toutiao_news/toutiao_backend/tests/test_users.py
@@ -2,19 +2,15 @@ from main import app
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
-def test_login():
-    response = client.post("/api/user/login",json={"username":"yuanshen","password":"123456"},headers={"Authorization":"Bearer ef0d78b0-95f2-494b-8295-f203ad53ebcb"})
-    assert response.status_code == 200
-
 def test_register():
-    response = client.post("/api/user/register",json={"username":"yuanshen3","password":"123456"})
+    response = client.post("/api/user/register",json={"username":"yuanshen4","password":"123456"})
     #打印完整错误信息
     print("status_code:", response.status_code)
     print("response json:", response.json())
     assert response.status_code == 200
 
-def test_get_user_info():
-    response = client.post("/api/users/info",headers={"Authorization":"Bearer ef0d78b0-95f2-494b-8295-f203ad53ebcb"})
-    print("status_code:", response.status_code)
-    print("response json:", response.json())
+def test_login():
+    response = client.post("/api/user/login",json={"username":"yuanshen","password":"123456"},headers={"Authorization":"Bearer ef0d78b0-95f2-494b-8295-f203ad53ebcb"})
     assert response.status_code == 200
+
+
